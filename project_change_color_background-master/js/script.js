8b82b654c21591c661fbfd2f47862e78dab73dba
GarrettKent <46579862+GarrettKent@users.noreply.github.com>
2020-04-20 13:29:48 -0500

    Add files via upload

---
+++ project_change_color_background-master/js/script.js
@@ -0,0 +1,16 @@
+//Choose a random color
+const button = document.querySelector('button')
+const body = document.querySelector('body')
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+
+
+//creates original background color
+body.style.backgroundColor = 'violet'
+
+//whenever button is clicked, changeBackground function is called
+button.addEventListener('click', changeBackground)
+
+function changeBackground(){
+    const colorIndex= parseInt(Math.random()*colors.length)
+    body.style.backgroundColor = colors[colorIndex]
+}
